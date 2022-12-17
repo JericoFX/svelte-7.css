316399d292d1aeb84226ddc679941f5c5dd7cd7a
JericoFX <blackjordison@gmail.com>
2022-12-16 21:07:32 -0300

    upgrade

--- src/main.ts
+++
@@ -1,8 +0,0 @@
-import App from './App.svelte';
-import '@fortawesome/fontawesome-free/css/all.css';
-import '7.css/dist/7.css';
-const app = new App({
-	target: document.getElementById('app'),
-});
-
-export default app;
