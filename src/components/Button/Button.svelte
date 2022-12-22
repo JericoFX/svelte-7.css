b4c19270a78a1827099f0560b004287781c73833
JericoFX <blackjordison@gmail.com>
2022-12-22 20:10:39 -0300

    Fix weird margin-top with buttons

--- src/components/Button/Button.svelte
+++ src/components/Button/Button.svelte
@@ -1,4 +1,7 @@
 <script lang="ts">
+	import {onMount} from 'svelte';
+
+	export let ref;
 	/**
 	 * @param {string} text - Text that will be in the button
 	 */
@@ -7,8 +10,13 @@
 	 * @param {boolean} disabled - check if the button will be disabled
 	 */
 	export let disabled = false;
+	onMount(() => {
+		if (ref.className === 'field-row') {
+			ref.style.margin = 0;
+		}
+	});
 </script>
 
-<div class="field-row">
+<div bind:this="{ref}" class="field-row">
 	<button on:click disabled="{disabled}">{text}</button>
 </div>
